6bec4a4f8a058ae5abc9670bb1d1a93389a945ee
Triagantaraga24 <90598532+Triagantaraga24@users.noreply.github.com>
2022-11-27 21:23:47 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+      <h2>By Raga Triagantara</h2>
+    </div>
+  );
+}
+
+export default App;
